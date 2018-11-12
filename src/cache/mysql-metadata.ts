7b8ca1fb3adabb8b7340fbb826aceddd88d05bcd
linyimin-bupt <linyimin520812@gmail.com>
2018-11-12 16:14:43 +0800

    Get sepecific source metadata

---
+++ src/cache/mysql-metadata.ts
@@ -0,0 +1,49 @@
+import { MysqlMetadata } from '../data-access/mysql-metadata'
+
+
+export interface FieldObj {
+  [name: string]: {
+    isNullAble      : string,
+    columnType      : string,
+    characterSetName: string,
+    columnKey       : string,
+    privileges      : string,
+    columnComment   : string,
+  }
+}
+export interface TableObj {
+  [tableName: string]: FieldObj
+}
+
+export interface DatabaseObj {
+  [databaseName: string]: TableObj,
+}
+
+export const metaDataCache = new Map<string, DatabaseObj>()
+
+export const syncMetaData = async (source: string) => {
+  const databases = await MysqlMetadata.getAllDatabases()
+  let databaseMetaData: DatabaseObj = {}
+  for (let i = 0; i < databases.length; i++) {
+    const tables = await MysqlMetadata.getAllTables(databases[i])
+    let tableMetaData: TableObj = {} as TableObj
+    for (let j = 0; j < tables.length; j++) {
+      const fields = await MysqlMetadata.getTableFields(databases[i], tables[j])
+      tableMetaData[tables[j]] = {}
+      fields.map(field => {
+
+        tableMetaData[tables[j]][field.columName] = {
+          isNullAble      : field.isNullAble,
+          columnType      : field.columnType,
+          characterSetName: field.characterSetName,
+          columnKey       : field.columnKey,
+          privileges      : field.privileges,
+          columnComment   : field.columnComment,
+        }
+
+      })
+    }
+    databaseMetaData[databases[i]] = tableMetaData
+  }
+  metaDataCache.set(source, databaseMetaData)
+}
