027ef439d4c581b96df6c0e8432bf90f64ea5aa7
linyimin-bupt <linyimin520812@gmail.com>
2018-11-28 15:08:45 +0800

    Change baseUrl of swagger docs

--- src/routes/swagger-ui.ts
+++ src/routes/swagger-ui.ts
@@ -22,7 +22,7 @@ export const getSwaggerJson = function (filename: string) {
           email: 'linyimin520812@gmail.com'
         }
       },
-      host: `localhost:5000`,
+      host: `123.56.87.74:5002`,
       basePath: '/',
     },
      apis: [path.join(__dirname, './*.ts')]
